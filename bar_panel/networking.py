1b53864a93d2e993e3156ad136adce0aed0c2674
Johannes Bittner <johannes.bittner@gmail.com>
2017-04-06 18:45:59 +0000

    use net.ini in bar_panel

--- bar_panel/networking.py
+++ bar_panel/networking.py
@@ -2,8 +2,7 @@ import socketserver
 import threading
 import time
 
-HOST = "0.0.0.0"
-PORT = 9999
+import config
 
 class StateTCPHandler(socketserver.StreamRequestHandler):
     def send_state(self):
@@ -36,8 +35,10 @@ class ThreadingTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 
 def init(akk_state):
     socketserver.TCPServer.allow_reuse_address = True
+    
+    (_, listen_port) = config.get_bar_raspberry_host_port()
+    server = ThreadingTCPServer(("0.0.0.0", listen_port), StateTCPHandler)
 
-    server = ThreadingTCPServer((HOST, PORT), StateTCPHandler)
     # see https://stackoverflow.com/questions/6875599/with-python-socketserver-how-can-i-pass-a-variable-to-the-constructor-of-the-han
     server.akk_state = akk_state
     server_thread = threading.Thread(target=server.serve_forever)
